30f7faf064722da6c8ada57df9015fdbb3aa6de3
Derek Hessinger <derekjhessinger@gmail.com>
2022-11-28 18:49:10 -0500

    updated tests

--- PQHeapTests.java
+++ PQHeapTests.java
@@ -32,7 +32,7 @@ public class PQHeapTests{
 			assert heap.size == 0 : "Error in PQHeap::PQHeap()";
 		}
 
-		// case 2: testing offer
+		// case 2: testing offer()
 		{
 			// setup
 			heap = heap;
@@ -42,7 +42,34 @@ public class PQHeapTests{
 			System.out.println(heap + " == 1: 1\n");
 
 			// test
-			assert System.out.println(heap).equals("1: 1\n") : "Error in PQHeap::offer()";
+			assert heap.size == 1 : "Error in PQHeap::offer()";
+		}
+
+		// case 3: testing peek()
+		{
+			// setup
+			heap = heap;
+			heap.offer(2);
+
+			// verify
+			System.out.println(heap.peek() + " == 2");
+
+			// test
+			assert heap.peek() == 2 : "Error in PQHeap::peek()";
+		}
+
+		// case 4: testing poll()
+		{
+			// setup
+			heap = heap;
+			for (int i = 3; i < 11; i++){
+				heap.offer(i);
+			}
+			int deleted = heap.poll();
+
+			// verify
+			System.out.println(deleted + " == 10");
+			System.out.println(heap);
 		}
 	}
 }
